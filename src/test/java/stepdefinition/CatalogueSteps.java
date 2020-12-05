9398b173fae64b90b6e3b3b7c91f526a82f13641
Uday Kant Naik <uday.naik@wissen.com>
2020-12-06 05:23:03 +0530

    Inital commit for catalogue test cases

---
+++ src/test/java/stepdefinition/CatalogueSteps.java
@@ -0,0 +1,40 @@
+package stepdefinition;
+
+import org.testng.Assert;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import main.CucumberRunner;
+import pages.CatalougePage;
+
+public class CatalogueSteps extends CucumberRunner{
+	
+	CatalougePage catalogue = new CatalougePage();
+	String itemName;
+	
+	@Given("^Item with name \"(.*?)\" is present on page (\\d+)$")
+	public void verifyCatakougeItemIsPresent(String name,int pageNumber) throws Throwable {
+		System.out.println("Inside Given");
+		itemName = name;
+		catalogue.goToPage(pageNumber);
+		Assert.assertTrue(catalogue.getCatalogueItem(name).isDisplayed(), "Catalogue is not present");
+		
+	}
+	
+	@Then("^Verify that items price should be (\\d+)$")
+	public void verifyPrice(String price) throws Throwable {
+		System.out.println("Inside Then 1");
+		Assert.assertEquals(catalogue.getPrice(itemName), price,"Price is not matching");
+	}
+	
+	@Then("^Add item can be added to cart$")
+	public void addItemToCart() throws Throwable{
+		System.out.println("Inside then 2");
+		catalogue.getAddtoBasketButton(itemName).click();
+	}
+	
+	
+
+}
+
+
---
+++ src/test/resources/features/catalogue.feature
@@ -0,0 +1,6 @@
+Feature: To test catalogue details
+
+	@TestngScenario
+	Scenario: Test catalogue
+  Given Item with name ".NET BOT BLACK SWEATSHIRT" is present on page {1} 
+    Then Add item can be added to cart 
